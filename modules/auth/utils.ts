a511d111378aa78bf3ffe7aafc489c6da6ad6322
El-Gazoly <elgazoly11@gmail.com>
2026-01-26 17:59:36 +0200

    Refactor cookie settings in authentication utility: streamline cookie attributes for production environments by conditionally applying sameSite, secure, and domain settings based on NODE_ENV, enhancing security and compatibility.

--- modules/auth/utils.ts
+++ modules/auth/utils.ts
@@ -10,16 +10,11 @@ export const generateAuthCookie =
   async () => {
     const cookies = await getCookies();
     const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
-    const isProduction = process.env.NODE_ENV === "production";
     cookies.set(`${prefix}-token`, value, {
       httpOnly: true,
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
-      sameSite: "none",
-      // When sameSite is "none", secure must be true
-      secure: true,
-      // Only set domain if it's defined and in production
-      ...(rootDomain && isProduction ? { domain: rootDomain } : {}),
+      ...(process.env.NODE_ENV !== "development" && { sameSite: "none", secure: true, domain: rootDomain } ),
     });
     return cookies;
   };
